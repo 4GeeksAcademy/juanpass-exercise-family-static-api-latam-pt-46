3515614535e1a349767c350c9d067c8743001eb1
juanpass <34844456+juanpass@users.noreply.github.com>
2025-05-03 04:12:11 +0000

    se corrige error al crear miembro

--- src/app.py
+++ src/app.py
@@ -13,11 +13,31 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
 
-# Handle/serialize errors like a JSON object
+Jane = {
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -30,16 +50,31 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_list_family():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+@app.route('/members', methods=['POST'])
+def post_member():
+    member = request.json
+    print("añadido", member)
+    jackson_family.add_member(member)
+    return member, 200
 
+@app.route('/members/<int:id>', methods=['GET'])
+def get_only_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"Error": "Miembro no encontrado"}), 404
+    return jsonify(member)
 
-# This only runs if `$ python src/app.py` is executed
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_only_member(id):
+    member = jackson_family.delete_member(id)
+    if member is None:
+        return jsonify({"Error": "Miembro no encontrado"}), 404
+    return jsonify({"done": True, "Delete_member": member}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -5,40 +5,35 @@ Update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            }
-        ]
+        self._members = []
 
-    # This method generates a unique incremental ID
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        member["last_name"] = self.last_name
+        member["id"] = self._generate_id()
+        member["lucky_numbers"] = list(member.get("lucky_numbers", set()))
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return member
+        return None
+
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        return next((m for  m in self._members if m["id"] == id), None)
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
